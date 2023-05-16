eac8fc46258cdc1cc731906b7cf3d9c0f73d53b5
francojb <fran.belingheri@hotmail.com>
2023-05-16 00:21:08 -0300

    prueba_backend

---
+++ src/main/java/com/portfolio/francoJb/FrancoJbApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.francoJb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrancoJbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrancoJbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/francoJb/FrancoJbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.francoJb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrancoJbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
